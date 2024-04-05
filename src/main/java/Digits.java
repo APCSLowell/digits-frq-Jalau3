422cc36cf1e653ddf734ba4e59718bbf175ffc92
Jalau3 <143014960+Jalau3@users.noreply.github.com>
2024-04-05 09:55:52 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -7,12 +7,20 @@ public class Digits
 
 	public Digits(int num)
 	{ /* to be implemented in part (a) */ 
-	    
+	    while(num>9){
+		    digitList.add(0,num%10);
+		    num = num/10;
+	    }
+		digitList.add(num);
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		for(int i = 0; i<digitList.size()-1; i++){
+			if(digitList.get(i)>digitList.get(i+1))
+				return false
+		}
+		return true;
 
 	}
 	
