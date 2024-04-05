41cd30b2e5c601b319c03e4224bfef8d93822485
Jalau3 <143014960+Jalau3@users.noreply.github.com>
2024-04-05 10:05:08 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -5,15 +5,15 @@ public class Digits
 
 	private ArrayList<Integer> digitList;
 
-	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    while(num>9){
-		    digitList.add(0,num%10);
-		    num = num/10;
-	    }
-		digitList.add(num);
-	}
-
+	  public Digits(int num)
+  { /* to be implemented in part (a) */ 
+      digitList = new ArrayList <Integer>();
+      while(num>9){
+        digitList.add(0,num%10);
+        num = (num/10);
+      }
+    digitList.add(0,num);
+  }
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
 		for(int i = 0; i<digitList.size()-1; i++){
